841a4ec85905dcee7f23ee4ded7682a501815398
Will-Gon <will.colocho@gmail.com>
2020-10-07 11:03:22 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  fetch('https://randomuser.me/api/') 
+  .then( res => res.json() ) 
+  .then( data => {
+    console.log(data["results"][0]["name"]["title"]) 
+    const firstName = data["results"][0]["name"]["first"]
+    const lastName = data["results"][0]["name"]["last"]
+    const title = data["results"][0]["name"]["title"]
+    const nameForm = document.querySelector("label")
+    const givenName = title+" ".concat(firstName)+" ".concat(lastName)
+    const nameP = document.createElement("p")
+    nameP.innerText = givenName
+    nameForm.appendChild(nameP)
+  })
 });
+
+
